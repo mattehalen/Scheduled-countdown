8b6b0738eb1a2e9633cb21120d519b21dd38e17e
mattehalen <matte_halen@hotmail.com>
2021-01-21 10:58:43 +0100

    Adding chose IP to MAIN window PART 1 - FRONT END

--- main.js
+++ main.js
@@ -61,15 +61,15 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      nodeIntegration: true
+  }
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
