3fd9f202c59b0db167fc795afca4591699fb5949
Mathias Halén <matte_halen@hotmail.com>
2021-05-26 13:17:12 +0200

    [Apple Push Notification] - Part 3
    Now sends Notification 5 4 3 2 1 before show start

--- src/APN/index.js
+++ src/APN/index.js
@@ -1,5 +1,9 @@
 var apn = require('apn');
-
+var SC = require('../websocket-listeners/SC-module/service');
+console.log("----------");
+console.log(SC);
+console.log("----------");
+//C:\Users\Admin\Documents\GitHub\My Projects\Electron\Scheduled-countdown\src\websocket-listeners\SC-module
 var options = {
   token: {
     key: "src/APN/AuthKey_WX4QX4S55M.p8",
@@ -11,8 +15,6 @@ var options = {
 var apnProvider = new apn.Provider(options);
 let deviceToken = "04ED2C021A346AB3DA6F162146A16A1A994102801B35BC17DF84F8870485AD3F"
 //-- iPhone Mathias = 04ED2C021A346AB3DA6F162146A16A1A994102801B35BC17DF84F8870485AD3F
-
-
 function sendNotification(minutes) {
   var messageString = minutes + "min to next number!"
   var note = new apn.Notification();
@@ -32,6 +34,42 @@ function sendNotification(minutes) {
     console.log("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
   });
 }
+
+
+async function checkCountDown(){
+  let dly = 2000
+  let countdown = await SC.countDown()
+  let ms   = countdown.countDownTimeInMS
+  let sec   = ms/1000
+
+  if (countdown.bool) {
+
+    if (sec > (-5*60-1) && sec < (-5*60) ) {
+      
+      setTimeout(() => { console.log("========================= 5"); }, dly);
+      sendNotification(5);
+    }
+    if (sec > (-4*60-1) && sec < (-4*60) ) {
+      setTimeout(() => { console.log("========================= 4"); }, dly);
+      sendNotification(4);
+    }
+    if (sec > (-3*60-1) && sec < (-3*60) ) {
+      setTimeout(() => { console.log("========================= 3"); }, dly);
+      sendNotification(3);
+    }
+    if (sec > (-2*60-1) && sec < (-2*60) ) {
+      setTimeout(() => { console.log("========================= 2"); }, dly);
+      sendNotification(2);
+    }
+    if (sec > (-1*60-1) && sec < (-1*60) ) {
+      setTimeout(() => { console.log("========================= 1"); }, dly);
+      sendNotification(1);
+    }
+  }
+  setTimeout(checkCountDown,(1000*1));
+}
+checkCountDown(checkCountDown)
+
 module.exports = {
   sendNotification
 }
