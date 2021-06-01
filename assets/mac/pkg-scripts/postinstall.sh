4dc98b04cca851ef4e324116b5ddbe1d95eed6a2
Mathias Halén <matte_halen@hotmail.com>
2021-06-01 23:07:50 +0200

    ----------> [Version Release] - V.0.0.11
    Try to release to Appstore

---
+++ assets/mac/pkg-scripts/postinstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+open -n -a /Applications/Scheduled-Countdown.app
+
+exit 0
---
+++ assets/mac/pkg-scripts/preinstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#open -n -a /Applications/Scheduled-Countdown.app
+$2 = /Applications
+
+exit 0
