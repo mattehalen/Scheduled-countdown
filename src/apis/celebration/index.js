a25986f6decdba70d2db34918549017baacacb1c
Mathias Halén <matte_halen@hotmail.com>
2021-09-24 15:03:39 +0200

    Basic Celebration displayt on /celebration

--- src/apis/celebration/index.js
+++ src/apis/celebration/index.js
@@ -5,30 +5,37 @@ const WebSocketService = require('./../../websocket/websocket-service');
 
 
 router.get('/', async function (req, res) {
+    var d = new Date()
+    const offset = d.getTimezoneOffset()
+    var yourDate = new Date(d.getTime() - (offset * 60 * 1000))
+    var date = yourDate.toISOString().split('T')[0]
+
+    var celebrationList = await Service.get()
     res.render('celebration', {
-        title: 'Scheduled-CountDown'
+        title: 'Scheduled-CountDown',
+        celebrationList: celebrationList[`${date}`]
     });
 });
 router.post('/submit', async function (req, res) {
     try {
         var dateObject = {}
         var celebrationObject = {}
-    
+
         var d = new Date()
         const offset = d.getTimezoneOffset()
         var yourDate = new Date(d.getTime() - (offset * 60 * 1000))
         var date = yourDate.toISOString().split('T')[0]
-    
-    
+
+
         var celebrationList = await Service.get()
-    
+
         console.log("----------> celebration/submit");
         let tableNr = req.body.tableNr;
         let name = req.body.name;
         let reason = req.body.reason;
         let years = req.body.years;
         let info = req.body.info;
-    
+
         celebrationObject = {
             tableNr: tableNr,
             name: name,
@@ -57,11 +64,11 @@ router.post('/submit', async function (req, res) {
                 celebrationList[`${date}`].forEach(element => {
                     if (element.name === celebrationObject.name && element.years === celebrationObject.years) {
                         duplicate += 1
-                     }
-                    
+                    }
+
                 })
-    
-                console.log("Duplicates = "+duplicate)
+
+                console.log("Duplicates = " + duplicate)
 
                 if (duplicate == 0) {
                     console.log("You SHould have got a new entry now")
@@ -77,7 +84,7 @@ router.post('/submit', async function (req, res) {
 });
 
 async function write(data) {
-    
+
     await Service.write(data)
 }
 
